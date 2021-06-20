3b64c88be7eaa0b545be9d155858782c569afd0c
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2021-06-20 22:05:30 +0800

    Update lc_512.sql

--- code_sample/lc_512.sql
+++ code_sample/lc_512.sql
@@ -1,4 +1,4 @@
-# Initial answer 
+# Solution 1 (self)
 SELECT 
 	player_id, 
 	device_id 
@@ -6,7 +6,7 @@ FROM Activity
 GROUP BY player_id
 HAVING  event_date = MIN(event_date);
 
-# Correct answer
+# Solution 2 (reference)
 select player_id, device_id
 from Activity
 where (player_id, event_date) in
