7cf5c352ca33caa60950232705c98c55ee17ab83
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2021-06-22 23:16:54 +0800

    Update lc_1241.sql

--- code_sample/lc_1241.sql
+++ code_sample/lc_1241.sql
@@ -12,6 +12,7 @@ insert into Submissions(sub_id, parent_id) values(9, 1);
 insert into Submissions(sub_id, parent_id) values(10, 2);
 insert into Submissions(sub_id, parent_id) values(6, 7);
 
+# Solution 1(self, correct)
 SELECT post.sub_id AS post_id, COUNT(DISTINCT comment.sub_id) AS number_of_comments
 FROM
 (SELECT DISTINCT sub_id
@@ -24,3 +25,12 @@ WHERE parent_id IS NOT NULL) comment
 ON post.sub_id = comment.parent_id 
 GROUP BY post.sub_id
 ORDER BY post.sub_id;
+
+# Solution 2(reference, correct)
+select s1.sub_id as post_id,
+    count(distinct s2.sub_id) as number_of_comments
+from Submissions s1
+left join Submissions s2
+on s2.parent_id = s1.sub_id
+where s1.parent_id is null
+group by s1.sub_id
