fc8c4241c37fb4c1f3a302630ecda13ccb3d7ad2
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2021-06-22 14:18:34 +0800

    Update lc_1173.sql

--- code_sample/lc_1173.sql
+++ code_sample/lc_1173.sql
@@ -3,15 +3,15 @@ create table Delivery(delivery_id integer, customer_id integer, order_date date,
 insert into Delivery(delivery_id, customer_id, order_date, customer_pref_delivery_date)
             values(1, 1, '2019-08-01', '2019-08-02');
 insert into Delivery(delivery_id, customer_id, order_date, customer_pref_delivery_date)
-            values(2, 5, '2019-08-02', '2019-08-02');
+            values(2, 2, '2019-08-02', '2019-08-02');
 insert into Delivery(delivery_id, customer_id, order_date, customer_pref_delivery_date)
             values(3, 1, '2019-08-11', '2019-08-12');
 insert into Delivery(delivery_id, customer_id, order_date, customer_pref_delivery_date)
-            values(4, 1, '2019-08-24', '2019-08-26');
+            values(4, 3, '2019-08-24', '2019-08-26');
 insert into Delivery(delivery_id, customer_id, order_date, customer_pref_delivery_date)
-            values(5, 1, '2019-08-21', '2019-08-22');
+            values(5, 3, '2019-08-21', '2019-08-22');
 insert into Delivery(delivery_id, customer_id, order_date, customer_pref_delivery_date)
-            values(6, 1, '2019-08-11', '2019-08-13');
+            values(6, 2, '2019-08-11', '2019-08-13');
 insert into Delivery(delivery_id, customer_id, order_date, customer_pref_delivery_date)
             values(7, 4, '2019-08-09', '2019-08-09');
 
