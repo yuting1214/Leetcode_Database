179fa05b243038a0157bf77d609092b56d34c013
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2021-06-26 18:58:10 +0800

    Create lc_1543.sql

---
+++ code_sample/lc_1543.sql
@@ -0,0 +1,8 @@
+# Solution 1(self, correct)
+SELECT
+    LOWER(TRIM(product_name)) AS product_name,
+    DATE_FORMAT(sale_date, '%Y-%m') AS sale_date,
+    COUNT(*) AS total  
+FROM Sales
+GROUP BY LOWER(TRIM(product_name)), DATE_FORMAT(sale_date, '%Y-%m')
+ORDER BY product_name, sale_date
