5517adf76827ecc39cbf12a9ba5b88e36800e39b
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2021-06-26 19:12:56 +0800

    Create lc_1565.sql

---
+++ code_sample/lc_1565.sql
@@ -0,0 +1,8 @@
+# Solution 1(self, correct
+SELECT
+  DATE_FORMAT(order_date , '%Y-%m') AS month, 
+  COUNT(order_id) AS order_count,
+  COUNT(DISTINCT customer_id) AS customer_count
+FROM Orders
+WHERE invoice > 20
+GROUP BY DATE_FORMAT(order_date , '%Y-%m')
