d3c24dc9580f747ac37a72f1a688d7312751dbf0
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2021-07-03 16:14:23 +0800

    Update lc_1159.sql

--- code_sample/lc_1159.sql
+++ code_sample/lc_1159.sql
@@ -1,3 +1,49 @@
+# Table
+## 1. Users
+CREATE TABLE Users
+(
+     user_id 	varchar(300),
+     join_date 	date,
+     favorite_brand 	varchar(255)
+);
+INSERT INTO Users ( user_id , join_date , favorite_brand )
+VALUES
+    ('1', '2019-01-01', 'Lenovo'),
+    ('2', '2019-02-09', 'Samsung'),
+    ('3', '2019-01-19', 'LG'),
+    ('4', '2019-05-21', 'HP');
+## 2. Orders
+CREATE TABLE Orders
+(
+     order_id 	varchar(300),
+     order_date 	date,
+     item_id 	varchar(300),
+     buyer_id 	varchar(300),
+     seller_id 	varchar(255)
+);
+INSERT INTO Orders ( order_id , order_date , item_id , buyer_id , seller_id )
+VALUES
+    ('1', '2019-08-01', '4', '1', '2'),
+    ('2', '2019-08-02', '2', '1', '3'),
+    ('3', '2019-08-03', '3', '2', '3'),
+    ('4', '2019-08-04', '1', '4', '2'),
+    ('5', '2019-08-04', '1', '3', '4'),
+    ('6', '2019-08-05', '2', '2', '4');
+## 3. Items
+CREATE TABLE Items 
+(
+     item_id 	varchar(300),
+     item_brand 	varchar(255)
+);
+
+INSERT INTO Items  ( item_id , item_brand )
+VALUES
+    ('1', 'Samsung'),
+    ('2', 'Lenovo'),
+    ('3', 'LG'),
+    ('4', 'HP');
+
+# Solution 1(self, correct)
 SELECT
   F.user_id,
   (CASE WHEN F.item_id = Second.second_item_id THEN 'yes' ELSE 'no' END) 2nd_item_fav_brand 
