0cb9e2c8e2429e05fefb51f6e14118a0fd48155b
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2022-01-03 04:39:05 +0800

    Update lc_1164.sql

--- code_sample/lc_1164.sql
+++ code_sample/lc_1164.sql
@@ -30,3 +30,38 @@ FROM Products
 WHERE change_date <= '2019-08-16'
 GROUP BY product_id)) changed_table
 ON u.product_id = changed_table.product_id
+
+# Solution 2(reference)
+select p1.product_id, p1.new_price as price
+from Products p1
+where p1.change_date <= '2019-08-16' and
+    (product_id, datediff('2019-08-16', p1.change_date)) in
+    (select product_id, min(datediff('2019-08-16', change_date))
+    from Products
+    where change_date <= '2019-08-16'
+    group by product_id)
+
+union
+
+select product_id, 10 as price
+from Products
+group by product_id
+having min(change_date) > '2019-08-16'
+order by price desc
+
+# Solution 3( revise reference)
+select p1.product_id, p1.new_price as price
+from Products p1
+where (p1.product_id, p1.change_date) IN 
+    (select product_id, max(change_date)
+    from Products
+    where change_date <= '2019-08-16'
+    group by product_id)
+    
+union
+
+select product_id, 10 as price
+from Products
+group by product_id
+having min(change_date) > '2019-08-16'
+order by price desc
