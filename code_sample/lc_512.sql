68bef086686d7defdbcf7ac3cae3eb95933b91dd
yuting1214 <31685881+yuting1214@users.noreply.github.com>
2022-01-02 08:21:29 +0800

    Update lc_512.sql

--- code_sample/lc_512.sql
+++ code_sample/lc_512.sql
@@ -1,4 +1,4 @@
-# Solution 1 (self)
+# Solution 1 (self, wrong)
 SELECT 
 	player_id, 
 	device_id 
@@ -13,3 +13,12 @@ where (player_id, event_date) in
     (select player_id, min(event_date)
     from Activity 
     group by player_id);
+
+# Solution 3 (self, review)
+SELECT a.player_id,	a.device_id
+FROM Activity a
+right JOIN
+(SELECT player_id, Min(event_date) as event_date
+FROM Activity
+GROUP BY player_id) b
+ON a.player_id = b.player_id AND a.event_date = b.event_date
